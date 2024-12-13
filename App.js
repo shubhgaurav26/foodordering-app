6b92bf82f39fa0cb2604169ab92b4080441cfb3b
Shubham <shubhgaurav.01@gmail.com>
2024-12-13 13:13:15 +0530

    test3

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (<h1 id = "heading">Hello world using jsx</h1>);
-console.log(jsxHeading);
 
 
-const HeadingComponent =()=>{
-    return<h1> Namaste React Functional Component </h1>
+
+
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className ="logo - container">
+                <img className ="logo" src =" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZB-sbuI_mSzHI3rvvC0ZXus0WGwDKq99frg&s"/>
+            </div>
+            <div className =" nav-items">
+                <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+
+                </ul>
+              
+
+            </div>
+
+        </div>
+
+    )
+    
+}
+
+const AppLayout = () => {
+    return <div className= "app">
+        <Header/>
+
+    </div>
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root")); 
-
-root.render(jsxHeading);
-
-/*
-Test 2
-*/
 
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
+root.render(<AppLayout/>);
