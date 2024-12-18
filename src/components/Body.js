d4160fdf2c3da8e0752625b4adbd516462aa90c9
Shubham <shubhgaurav.01@gmail.com>
2024-12-19 00:11:14 +0530

    filter by rating

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,8 @@ import resList from "../../utils/mockData";
 const Body =() =>{
     return(
         <div className="body">
-            <div className="search "> Search </div>
+            <div className="filter ">
+                 <button className="filter-btn" onClick={()=>{console.log("Button Clicked");}}>Top Rated Restaurent</button> </div>
             <div className="res-container "> 
               {
                 resList.map((restaurant) => (
