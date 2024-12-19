ed03ab3e7a611bccff536b7b987734577a99003c
Shubham <shubhgaurav.01@gmail.com>
2024-12-20 02:50:31 +0530

    apicall

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,56 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
+import { useEffect, useState } from "react";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(()=>{
+
+    fetchData();
+},[]);
+
+const fetchData =async()=>{
+    const data = await fetch( 
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
+    );
+
+    const json = await data.json();
+    console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    //Optionl Chaining
+    setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    // setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    
+  
+};
 
 
-
-const Body =() =>{
-    return(
-        <div className="body">
-            <div className="filter ">
-                 <button className="filter-btn" onClick={()=>{console.log("Button Clicked");}}>Top Rated Restaurent</button> </div>
-            <div className="res-container "> 
-              {
-                resList.map((restaurant) => (
-                <RestaurantCard key ={restaurant.info.id} resData={restaurant}/>
-              ))}
-               
-         
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+ 
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
