79bb33ff80bb811b7896bca889387ebedb9a7a34
Shubham <shubhgaurav.01@gmail.com>
2024-12-20 15:22:36 +0530

    Login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import { LOGO_URL } from "../../utils/constants";
+import { useState } from "react";
 
 
 
 const Header = () => {
+
+
+
+    const [btnNameReact, setBtnNameReact] = useState("Login");
     return (
         <div className="header">
             <div className ="logo - container">
@@ -14,6 +19,11 @@ const Header = () => {
                 <li>About Us</li>
                 <li>Contact Us</li>
                 <li>Cart</li>
+ 
+                <button className="login" onClick={() => { setBtnNameReact("Logout"); }}>
+                {btnNameReact}
+              </button>
+              
                 </ul>             
             </div>
         </div>
