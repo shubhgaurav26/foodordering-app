09aba7e3962ac98917671f82b260163d67d36c08
anish2210 <anishjaiswal1220@gmail.com>
2024-12-28 21:32:33 +0530

    Changes made

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // You can specify a custom port here
+    proxy: {
+      '/api': {
+        target: 'https://www.swiggy.com', // The target API domain
+        changeOrigin: true, // Ensures the host header matches the target
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove `/api` prefix when forwarding
+      },
+    },
   },
 });
